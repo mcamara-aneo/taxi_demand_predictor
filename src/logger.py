80c60e6d138f851ff2a8eca88ed5dba0b76ddcea
Mamadou Camara <mcamara@aneo.fr>
2023-03-31 15:59:09 +0200

    Add new features

---
+++ src/logger.py
@@ -0,0 +1,10 @@
+import logging
+
+def get_logger() -> logging.Logger:
+    """Returns a logger
+    Returns:
+        logging.Logger: _description_
+    """
+    logger = logging.getLogger('dataflow')
+    logger.setLevel(logging.INFO)
+    return logger
